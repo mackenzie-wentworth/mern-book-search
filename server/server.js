82dc26a1889ea882822e6520173c8c16cabb8b46
mackenzie-wentworth <mwentworth28@gmail.com>
2023-06-26 11:55:17 -0400

    refactored server/server.js to implement Apollo server and apply it to the Express server as middleware; adjusted other content in server.js too; added comments with MW

--- server/server.js
+++ server/server.js
@@ -1,10 +1,26 @@
 const express = require('express');
+// MW - Import the ApolloServer class
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+
+// MW - require middleware
+const { authMiddleware } = require('./utils/auth');
+
+// MW - Import the two parts of a GraphQL schema
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
+
+// const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// MW - add 'authMiddleware' to Apollo server as "context" --> to configure Apollo with authorization middleware
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +30,28 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
+
+// app.use(routes);
+
+// db.once('open', () => {
+//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// });
+
+// MW - Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async () => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+};
+
+// MW - Call the async function to start the server
+startApolloServer();
