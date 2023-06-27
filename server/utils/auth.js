42e58b6124c50c78bd36ce3340929533bb12f096
mackenzie-wentworth <mwentworth28@gmail.com>
2023-06-26 21:43:53 -0400

    updated server/auth.js file so that the auth middleware function works with graphQL API; added refactor comments with MW

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,9 +6,11 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+  // MW - refactored auth middleware function
+  authMiddleware: function ({ req }) {
+    // allows token to be sent via req.query or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
+    
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -16,7 +18,8 @@ module.exports = {
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      // MW - if there's NOT a token, then we are just going to send the request back
+      return req;
     }
 
     // verify token and get user data out of it
@@ -25,11 +28,11 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
     }
 
     // send to next endpoint
-    next();
+    // MW - return the request object so it can be passed to the resolver as `context`
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
