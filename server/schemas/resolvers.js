d7fc637bc5a06001d33a0dc0653ea742d41e5aa9
mackenzie-wentworth <mwentworth28@gmail.com>
2023-06-26 21:54:06 -0400

    added 'schemas' directory based on instructions to include 'index.js' and 'resolvers.js' files; added content to schemas/index.js; added initial content to schemas/resovlers.js but need to finish query and mutations

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,15 @@
+// TODO: Define the query and mutation functionality to work with the Mongoose models
+
+const { User, Book } = require('../models');
+
+const resolvers = {
+    Query: {
+
+    },
+
+    Mutation: {
+        
+    },
+  };
+  
+  module.exports = resolvers;
